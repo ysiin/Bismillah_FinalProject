cc8f62cbd81abe17a53492a86a2e0abb846288f5
sulthanrizal <surie0312@gmail.com>
2025-09-22 00:10:11 +0700

    setting tailwind and route

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./page/home.tsx";
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Home,
+  },
+  {
+    path: "/pinjam",
+    Component: Home,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
