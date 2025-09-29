5f4771dd4b3ec82b0a9ae1d7bbfdb647b6e6bbb2
sulthanrizal <surie0312@gmail.com>
2025-09-29 18:01:41 +0700

    add logic active in header

--- fe/src/components/header/index.tsx
+++ fe/src/components/header/index.tsx
@@ -16,7 +16,7 @@ export default function Header() {
           <Link to="/">
             {currentPath === "/" ? (
               <div className="flex flex-row px-4 py-2 items-center gap-2.5">
-                <div className="bg-[#0C56F2] p-2">
+                <div className="bg-[#0C56F2] rounded-2xl p-2">
                   <HouseIcon className="text-white" />
                 </div>
                 <li className="text-accent-50 font-semibold">Beranda</li>
@@ -33,7 +33,7 @@ export default function Header() {
           <Link to="/category">
             {currentPath === "/category" ? (
               <div className="flex flex-row px-4 py-2 items-center gap-2.5">
-                <div className="bg-[#0C56F2] p-2">
+                <div className="bg-[#0C56F2] rounded-2xl p-2">
                   <ChartBarStacked className="text-white" />
                 </div>
                 <li className="text-accent-50 font-semibold">Category</li>
@@ -50,7 +50,7 @@ export default function Header() {
           <Link to="/list-booking">
             {currentPath === "/list-booking" ? (
               <div className="flex flex-row px-4 py-2 items-center gap-2.5">
-                <div className="bg-[#0C56F2] p-2">
+                <div className="bg-[#0C56F2] rounded-2xl p-2">
                   <BookCheck className="text-white" />
                 </div>
                 <li className="text-accent-50 font-semibold">List Booking</li>
