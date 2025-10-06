9fc370a8c5ef211fa1f5104e2cf08388db055274
ysiin <zachbone0@gmail.com>
2025-10-06 21:47:28 +0700

    add db book n category

--- be/app/Models/Book.php
+++ be/app/Models/Book.php
@@ -9,12 +9,15 @@ class Book extends Model
     protected $fillable = [
         'title',
         'author',
+        'description',
         'publisher',
         'year_published',
         'isbn',
         'category_id',
         'total_copies',
-        'available_copies'
+        'available_copies',
+        'total_pages',
+        'ratings',
     ];
 
     public function category()
