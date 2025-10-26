0f71349a7d321fcfdbb8f916fb875f7d2d82171e
ysiin <zachbone0@gmail.com>
2025-10-26 18:52:10 +0700

    middleware admin n anggota

--- be/routes/api.php
+++ be/routes/api.php
@@ -7,19 +7,39 @@ use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+//auth
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::get('/borrows', [BorrowController::class, 'index']);
+//all access
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/borrows', [BorrowController::class, 'store']);
+    Route::put('/borrows/{id}', [BorrowController::class, 'update']);
+    Route::delete('/borrows/{id}', [BorrowController::class, 'destroy']);
+
+
+
+    //admin only
+    Route::middleware('role:admin')->group(function () {
+
+        //books
+        Route::post('/books', [BookController::class, 'store']);
+        Route::put('/books/{id}', [BookController::class, 'update']);
+        Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+        //categories
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::put('/categories/{id}', [CategoryController::class, 'update']);
+        Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    });
 });
-
-
-Route::apiResource('books', BookController::class);
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('borrows', BorrowController::class);
-
-
