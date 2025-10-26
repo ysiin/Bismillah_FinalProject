a99a7057f1897ddafc8e877531824d5cb7216031
sulthanrizal <surie0312@gmail.com>
2025-10-26 18:57:52 +0700

    add index in map

--- fe/src/components/borrow-list/Booklist.tsx
+++ fe/src/components/borrow-list/Booklist.tsx
@@ -1,15 +1,19 @@
-import BookCard from "./BookCard"
+import BookCard from "./BookCard";
 
 export default function BookList({ books }: { books: any[] }) {
   if (!books || books.length === 0) {
-    return <p className="text-gray-500 text-center font-bold text-2xl pt-10">Belum ada buku yang sedang dipinjam.</p>
+    return (
+      <p className="text-gray-500 text-center font-bold text-2xl pt-10">
+        Belum ada buku yang sedang dipinjam.
+      </p>
+    );
   }
 
   return (
     <div className="flex flex-col gap-4 p-4">
-      {books.map((book) => (
-        <BookCard key={book.id} book={book} />
+      {books.map((book, index) => (
+        <BookCard key={index} book={book} />
       ))}
     </div>
-  )
+  );
 }
