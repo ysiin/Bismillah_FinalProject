9c52a4f2d25a0c4ac573e3cc55d23268065917b5
Akbar Ghazali <akb.agh123@gmail.com>
2025-10-26 20:56:09 +0700

    Fixing list-booking

--- fe/src/components/borrow-list/BookCard.tsx
+++ fe/src/components/borrow-list/BookCard.tsx
@@ -8,9 +8,24 @@ interface Book {
   rating: number
   category: string
   description: string
+  due_date: string
+  returned_at: string | null
 }
 
 export default function BookCard({ book }: { book: Book }) {
+  const formatDate = (dateStr: string) => {
+    const date = new Date(dateStr)
+    return date.toLocaleDateString("id-ID", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    })
+  }
+
+  const statusTanggal = book.returned_at
+    ? `Telah dikembalikan (${formatDate(book.returned_at)})`
+    : `Batas Pengembalian: ${formatDate(book.due_date)}`
+
   return (
     <div className="bg-white border flex p-2 rounded-xl">
       <BookCover />
@@ -32,7 +47,10 @@ export default function BookCard({ book }: { book: Book }) {
         </div>
 
         <p className="text-lg font-semibold mt-2">{book.category}</p>
+        <div className="flex justify-between">
         <p className="pt-4 text-gray-600">{book.description}</p>
+        <p id="due_date" className="pt-22 text-gray-200">{statusTanggal} </p>
+        </div>
       </div>
     </div>
   )
